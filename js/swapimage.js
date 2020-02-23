eedde8486476f0087ff8565f651b1459db9d842e
anhtu6 <anhtu632@gmail.com>
2020-02-23 12:34:13 +0700

    game

---
+++ js/swapimage.js
@@ -0,0 +1,60 @@
+function swap1() {
+    let x=Math.floor(Math.random()*3+1);
+    if(x==2){
+        document.getElementById('f1').src='img/monkey_part1x1.jpg';
+    }else{
+        if(x==3){
+            document.getElementById('f1').src='img/panda_swap_part1x1.jpg';
+        }else{
+            document.getElementById('f1').src='img/funny-cat1_part1x1.jpg';
+        }
+    }
+}
+function swap2() {
+    let x=Math.floor(Math.random()*3+1);
+    if(x==2){
+        document.getElementById('f2').src='img/monkey_part2x1.jpg';
+    }else{
+        if(x==3){
+            document.getElementById('f2').src='img/panda_swap_part2x1.jpg';
+        }else{
+            document.getElementById('f2').src='img/funny-cat1_part2x1.jpg';
+        }
+    }
+}
+function swap3() {
+    let x=Math.floor(Math.random()*3+1);
+    if(x==2){
+        document.getElementById('f3').src='img/monkey_part3x1.jpg';
+    }else{
+        if(x==3){
+            document.getElementById('f3').src='img/panda_swap_part3x1.jpg';
+        }else{
+            document.getElementById('f3').src='img/funny-cat1_part3x1.jpg';
+        }
+    }
+}
+function swap4() {
+    let x=Math.floor(Math.random()*3+1);
+    if(x==2){
+        document.getElementById('f4').src='img/monkey_part4x1.jpg';
+    }else{
+        if(x==3){
+            document.getElementById('f4').src='img/panda_swap_part4x1.jpg';
+        }else{
+            document.getElementById('f4').src='img/funny-cat1_part4x1.jpg';
+        }
+    }
+}
+function swap5() {
+    let x=Math.floor(Math.random()*3+1);
+    if(x==2){
+        document.getElementById('f5').src='img/monkey_part5x1.jpg';
+    }else{
+        if(x==3){
+            document.getElementById('f5').src='img/panda_swap_part5x1.jpg';
+        }else{
+            document.getElementById('f5').src='img/funny-cat1_part5x1.jpg';
+        }
+    }
+}
